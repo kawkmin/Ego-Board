5936a81f98ff547cc0e1126f16c7808ba219470c
kawksung <alstjd5479@naver.com>
2023-05-13 16:29:23 +0900

    feat: user가 pet의 연관관계의 주인으로 설정

--- src/main/java/com/self/egoboard/domain/user/entity/User.java
+++ src/main/java/com/self/egoboard/domain/user/entity/User.java
@@ -2,13 +2,17 @@ package com.self.egoboard.domain.user.entity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import com.self.egoboard.domain.pet.entity.Pet;
 import com.self.egoboard.domain.user.entity.embedded.Address;
 import com.self.egoboard.global.Entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -40,6 +44,10 @@ public class User extends BaseEntity {
 
   @Column(name = "nickname", nullable = false, length = MAX_NICKNAME_LENGTH)
   private String nickname;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "pet_id")
+  private Pet pet;
 
   @Embedded
   private Address address;
