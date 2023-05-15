825280e5b14479b3d00a436209cbc77d68a6cb19
kawksung <alstjd5479@naver.com>
2023-05-15 15:06:08 +0900

    style: `userRepository`->`UserRepository`

--- src/main/java/com/self/egoboard/domain/user/dao/UserRepository.java
+++ src/main/java/com/self/egoboard/domain/user/dao/UserRepository.java
@@ -3,6 +3,6 @@ package com.self.egoboard.domain.user.dao;
 import com.self.egoboard.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
