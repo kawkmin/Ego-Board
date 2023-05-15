85b192b60944b41ef3634cdab85d60adcf88cac0
kawksung <alstjd5479@naver.com>
2023-05-15 16:58:17 +0900

    feat: 주소에 컬럼명 및 검증 추가

--- src/main/java/com/self/egoboard/domain/user/entity/embedded/Address.java
+++ src/main/java/com/self/egoboard/domain/user/entity/embedded/Address.java
@@ -1,17 +1,35 @@
 package com.self.egoboard.domain.user.entity.embedded;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
+  private final String CITY_EMPTY = "도시를 입력해주세요!";
+  private final String STRETT_EMPTY = "거리를 입력해주세요!";
+  private final String ZIPCODE_EMPTY = "우편번호를 입력해주세요!";
+  private final String ZIPCODE_INVALID = "우편번호는 숫자로 이뤄진, 6자리 수 입니다.";
+  @NotEmpty(message = CITY_EMPTY)
+  @Column(name = "city", nullable = false)
   private String city;
+
+  @NotEmpty(message = STRETT_EMPTY)
+  @Column(name = "street", nullable = false)
   private String street;
+
+  @NotEmpty(message = ZIPCODE_EMPTY)
+  @Pattern(regexp = "^[0-9]*${6}", message = ZIPCODE_INVALID)
+  @Column(name = "zipcode", nullable = false)
   private String zipcode;
 
+  @Builder
   public Address(String city, String street, String zipcode) {
     this.city = city;
     this.street = street;
