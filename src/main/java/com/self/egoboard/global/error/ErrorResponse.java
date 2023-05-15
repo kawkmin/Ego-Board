56220c8278f534583f6a2e73fe451f972837e655
kawksung <alstjd5479@naver.com>
2023-05-15 23:22:16 +0900

    feat: 응답 포맷 정형화 하는 ErrorResponse 구현

---
+++ src/main/java/com/self/egoboard/global/error/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.self.egoboard.global.error;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse {
+
+  private final String message;
+
+  public static ErrorResponse from(String message) {
+    return new ErrorResponse(message);
+  }
+}
