8c6591e47ebb0d8ea5397011773ca500d404a456
kawksung <alstjd5479@naver.com>
2023-05-15 23:20:49 +0900

    feat: ErrorCode를 받는 모든 비지니스 예외 클래스 생성

---
+++ src/main/java/com/self/egoboard/global/error/BusinessException.java
@@ -0,0 +1,16 @@
+package com.self.egoboard.global.error;
+
+import org.springframework.http.HttpStatus;
+
+public class BusinessException extends RuntimeException {
+
+  private final HttpStatus httpStatus;
+  private final String message;
+
+  public BusinessException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.httpStatus = errorCode.getHttpStatus();
+    this.message = errorCode.getMessage();
+  }
+
+}
