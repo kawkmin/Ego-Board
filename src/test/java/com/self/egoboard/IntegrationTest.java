31a105501f81ae6a694903b5d454636cec6ca013
kawksung <alstjd5479@naver.com>
2023-05-16 10:52:19 +0900

    test: 통합 테스트 설정

---
+++ src/test/java/com/self/egoboard/IntegrationTest.java
@@ -0,0 +1,49 @@
+package com.self.egoboard;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import com.self.egoboard.domain.post.dao.PostRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@ExtendWith({RestDocumentationExtension.class})
+@Transactional
+@SpringBootTest
+public class IntegrationTest {
+
+  @Autowired
+  protected WebApplicationContext webApplicationContext;
+
+  @PersistenceContext
+  protected EntityManager em;
+
+  protected MockMvc mockMvc;
+
+  @BeforeEach
+  protected void setUpAll(RestDocumentationContextProvider restDocumentationContextProvider) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .addFilter(new CharacterEncodingFilter("UTF-8", true))
+        .apply(documentationConfiguration(restDocumentationContextProvider)
+            .operationPreprocessors()
+            .withRequestDefaults(
+                modifyUris().scheme("https").host("docs.api.com").removePort(), prettyPrint())
+            .withResponseDefaults(prettyPrint())
+        )
+        .build();
+  }
+
+}
